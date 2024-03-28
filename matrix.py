5bd7981687a261b178f2bee90a91db0187670f7d
Ishwor <dulalishan3@gmail.com>
2024-03-28 13:55:45 +0545

    added a script that prints a 3x3 matrix using list comprehension

--- matrix.py
+++ matrix.py
@@ -1,20 +1,8 @@
-#3x3 Matrix with list comprehension
-# i = int(input())
-# j = int(input())
-# k = int(input())
+#3x3 Matrix using list comprehension
 
-# list1 =[]
-# list2 =[]
-# for i in range(1,9,3):
-#     for j in range(3):
-#         list1.append(j+i)
-#     list2.append(list1)
-#     list1 =[]
-# print(list2)
-
-def matrixx():
+def matrix():
     return [ [i, i+1, i+2] for i in range(1,9,3) ]
-print(matrixx())
+print(matrix())
 
 
 
